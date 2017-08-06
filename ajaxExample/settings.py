fbf7e8c3e160bd6b0d9ff6b76fa8e2e3d3b0f026
zWind <zpsyhapcst@gmail.com>
2017-08-06 10:26:47 +0800

    add templates path to settings.py

--- ajaxExample/settings.py
+++ ajaxExample/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ajaxExample.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'ajax/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
